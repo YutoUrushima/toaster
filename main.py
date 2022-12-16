8da62a742f8156aee35963a8d1321b656effaa70
YutoUrushima <yuto259233@gmail.com>
2022-12-16 22:03:38 +0900

    Setup project

---
+++ main.py
@@ -0,0 +1,21 @@
+from plyer import notification
+
+notification.notify(
+  title="通知バナー",
+  message="メッセージ",
+  app_name="アプリ名",
+  app_icon="C:\office\office.ico",
+  timeout=10
+)
+
+# def main():
+#   notification.notify(
+#     title="通知バナー",
+#     message="メッセージ",
+#     app_name="アプリ名",
+#     app_icon="C:\office\office.ico",
+#     timeout=10
+#   )
+
+# if __name__ == "__main__":
+#   main()
