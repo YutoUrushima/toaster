b23dd5a4927387d6f3df27d5242de9f036377d4a
YutoUrushima <yuto259233@gmail.com>
2022-12-17 18:59:50 +0900

    Fix variable name

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ INTERVAL = 1 * 100 * 60 # 1 minute
 def main():
   # When the file is called, Requests the user to enter.
   name = input("Enter toast name: ")
-  time = input("Enter toast time: ")
+  notification_time = input("Enter toast time: ")
   ttl = input("Enter time for the toast to stay alive: ")
 
   # Create an instance of the Toaster class based on the input information.
-  toast = toaster.Toaster(name, time, ttl)
+  toast = toaster.Toaster(name, notification_time, ttl)
   print(f'Request accepted! Toast name: {toast.name}, Time: {toast.time}')
 
   # Every minute, check the current time and execute a toast notification if it is the same as the set time.
