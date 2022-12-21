d3465f8c3e104a040b2853bcf84127376c7a47f0
YutoUrushima <yuto259233@gmail.com>
2022-12-21 23:04:49 +0900

    Add prettier

--- main.py
+++ main.py
@@ -3,25 +3,27 @@ import time
 import datetime
 import schedule
 
-INTERVAL = 1 * 100 * 60 # 1 minute
+INTERVAL = 1 * 100 * 60  # 1 minute
+
 
 def main():
-  # When the file is called, Requests the user to enter.
-  name = input("Enter toast name: ")
-  notification_time = input("Enter toast time: ")
-  ttl = input("Enter time for the toast to stay alive: ")
+    # When the file is called, Requests the user to enter.
+    name = input("Enter toast name: ")
+    notification_time = input("Enter toast time: ")
+    ttl = input("Enter time for the toast to stay alive: ")
 
-  # Create an instance of the Toaster class based on the input information.
-  toast = toaster.Toaster(name, notification_time, ttl)
-  print(f'Request accepted! Toast name: {toast.name}, Time: {toast.time}')
+    # Create an instance of the Toaster class based on the input information.
+    toast = toaster.Toaster(name, notification_time, ttl)
+    print(f"Request accepted! Toast name: {toast.name}, Time: {toast.time}")
 
-  # Every minute, check the current time and execute a toast notification if it is the same as the set time.
-  while(True):
-    if datetime.datetime() == toast.time:
-      toast.call()
-      break
-    else:
-      time.sleep(INTERVAL)
+    # Every minute, check the current time and execute a toast notification if it is the same as the set time.
+    while True:
+        if datetime.datetime() == toast.time:
+            toast.call()
+            break
+        else:
+            time.sleep(INTERVAL)
+
 
 # def check():
 #   if datetime.datetime() == toast.time:
@@ -35,4 +37,4 @@ def main():
 #   sleep(1)
 
 if __name__ == "__main__":
-  main()
+    main()
--- toaster.py
+++ toaster.py
@@ -1,18 +1,15 @@
 from plyer import notification
 
+
 class Toaster:
-  def __init__(self, name, time, ttl):
-    self.name = name
-    self.time = time
-    self.ttl = ttl
-  
-  def register(self):
-    # register to MongoDB
-    print('registerd!')
-  
-  def call(self):
-    notification.notify(
-      title = "toaster",
-      message = self.name,
-      timeout = self.ttl
-    )
+    def __init__(self, name, time, ttl):
+        self.name = name
+        self.time = time
+        self.ttl = ttl
+
+    def register(self):
+        # register to MongoDB
+        print("registerd!")
+
+    def call(self):
+        notification.notify(title="toaster", message=self.name, timeout=self.ttl)
