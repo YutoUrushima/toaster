11c1fd1aa4704a64aed09ac50bb1bcd304f1c798
YutoUrushima <yuto259233@gmail.com>
2022-12-22 19:30:39 +0900

    Add redis

--- toaster.py
+++ toaster.py
@@ -1,4 +1,5 @@
 from plyer import notification
+import redis
 
 
 class Toaster:
@@ -6,10 +7,10 @@ class Toaster:
         self.name = name
         self.time = time
         self.ttl = ttl
+        self.redis = redis.Redis(host="localhost", post=6379, db=0)
 
     def register(self):
-        # register to MongoDB
-        print("registerd!")
+        self.redis.set(self.name, self.time)
 
     def call(self):
         notification.notify(title="toaster", message=self.name, timeout=self.ttl)
