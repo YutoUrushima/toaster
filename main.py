6795c050480b3ab40f44b33d941c7a7d8428c961
YutoUrushima <yuto259233@gmail.com>
2022-12-22 20:01:15 +0900

    integrate Redis server

--- main.py
+++ main.py
@@ -2,27 +2,31 @@ import toaster
 import time
 import datetime
 import schedule
+import redis
 
 INTERVAL = 1 * 100 * 60  # 1 minute
 
 
-def main():
+def main(redis):
     # When the file is called, Requests the user to enter.
     name = input("Enter toast name: ")
     notification_time = input("Enter toast time: ")
     ttl = input("Enter time for the toast to stay alive: ")
 
     # Create an instance of the Toaster class based on the input information.
-    toast = toaster.Toaster(name, notification_time, ttl)
+    toast = toaster.Toaster(name, notification_time, ttl, redis)
+    toast.register()
     print(f"Request accepted! Toast name: {toast.name}, Time: {toast.time}")
 
+    print(toast.fetch("toast").decode("utf-8"))
+
     # Every minute, check the current time and execute a toast notification if it is the same as the set time.
-    while True:
-        if datetime.datetime() == toast.time:
-            toast.call()
-            break
-        else:
-            time.sleep(INTERVAL)
+    # while True:
+    #     if datetime.datetime() == toast.time:
+    #         toast.call()
+    #         break
+    #     else:
+    #         time.sleep(INTERVAL)
 
 
 # def check():
@@ -37,4 +41,5 @@ def main():
 #   sleep(1)
 
 if __name__ == "__main__":
-    main()
+    redis = redis.Redis(host="localhost", port=6379, db=0)
+    main(redis)
--- toaster.py
+++ toaster.py
@@ -1,16 +1,18 @@
 from plyer import notification
-import redis
 
 
 class Toaster:
-    def __init__(self, name, time, ttl):
+    def __init__(self, name, time, ttl, redis):
         self.name = name
         self.time = time
         self.ttl = ttl
-        self.redis = redis.Redis(host="localhost", post=6379, db=0)
+        self.redis = redis
 
     def register(self):
         self.redis.set(self.name, self.time)
+
+    def fetch(self, name):
+        return self.redis.get(name)
 
     def call(self):
         notification.notify(title="toaster", message=self.name, timeout=self.ttl)
