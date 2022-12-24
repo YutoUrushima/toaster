bf6c79d96d5235cbf5d7e67c5c4cbfe1736d8513
YutoUrushima <yuto259233@gmail.com>
2022-12-24 23:28:52 +0900

    Support by datetime input

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ INTERVAL = 1 * 100 * 60  # 1 minute
 def main(redis):
     # When the file is called, Requests the user to enter.
     name = input("Enter toast name: ")
-    notification_time = input("Enter toast time: ")
+    notification_time = input("Enter toast time ex)2022 12 24 15 00: ")
     ttl = input("Enter time for the toast to stay alive: ")
 
     # Create an instance of the Toaster class based on the input information.
--- toaster.py
+++ toaster.py
@@ -1,18 +1,31 @@
 from plyer import notification
+import re
+
+DATETIME_REGEXP = (
+    r"\d{4}\s(0[1-9]|1[0-2])\s(0[1-9]|[12]\d|3[01])\s(0[1-9]|1\d|2[0-3])\s[0-5]\d"
+)
+# TODAY_REGEXP = r"today\s(0[1-9]|1\d|2[0-3])\s[0-5]\d"
+# TOMORROW_REGEXP = r"tomorrow\s(0[1-9]|1\d|2[0-3])\s[0-5]\d"
 
 
 class Toaster:
     def __init__(self, name, time, ttl, redis):
+        if not re.fullmatch(DATETIME_REGEXP, time):
+            raise
         self.name = name
         self.time = time
         self.ttl = ttl
         self.redis = redis
 
     def register(self):
-        self.redis.set(self.name, self.time)
+        self.redis.hset(f"Toast:{self.name}", "name", self.name)
+        self.redis.hset(f"Toast:{self.name}", "time", self.time)
+        self.redis.hset(f"Toast:{self.name}", "ttl", self.ttl)
 
+    @classmethod
     def fetch(self, name):
-        return self.redis.get(name)
+        return self.redis.get(f"Toast:{name}")
 
+    @classmethod
     def call(self):
         notification.notify(title="toaster", message=self.name, timeout=self.ttl)
