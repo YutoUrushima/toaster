e401c53e061f107abfe8766dcade600e7a4b0339
YutoUrushima <yuto259233@gmail.com>
2023-01-08 17:47:16 +0900

    Refactor input process & Add validation for user input

--- toaster/main.py
+++ toaster/main.py
@@ -2,16 +2,20 @@ from toast import Toast
 
 
 def main():
-    # When the file is called, Requests the user to enter.
+    # Input toast time
     name = input("Enter toast name: ")
     message = input("Enter additional messages If you have them: ")
-    notification_time = input("Enter toast time ex)2022 12 24 15 00: ")
+    notification_time = input(
+        "Enter toast time in the format 'yyyy mm dd HH mm': "
+    )  # ex)2022 12 24 15 00
     ttl = input("Enter time for the toast to stay alive: ")
 
-    # Create an instance of the Toaster class based on the input information.
+    # Record the toast
     toast = Toast(name, message, notification_time, ttl)
     toast.register()
-    print(f"Request accepted! Toast name: {name}, Time: {notification_time}")
+    print(
+        f"Request accepted! Toast name: {name}, messgae: {message}, Time: {notification_time}"
+    )
 
 
 if __name__ == "__main__":
--- toaster/toast.py
+++ toaster/toast.py
@@ -1,24 +1,43 @@
 import re
 import redis
+import json
+import sys
 
 
 DATETIME_REGEXP = (
     r"\d{4}\s(0[1-9]|1[0-2])\s(0[1-9]|[12]\d|3[01])\s(0[1-9]|1\d|2[0-3])\s[0-5]\d"
 )
-LOCALHOST = "localhost"
-PORT = 6379
-DB = 0
+TTL_REGEXP = r"\d?\d"
 
 
 class Toast:
     def __init__(self, name, message, time, ttl):
+        if not name:
+            print("Please enter some characters as a name.")
+            sys.exit()
         if not re.fullmatch(DATETIME_REGEXP, time):
-            raise
+            print(
+                "Date and time format is incorrect. Please enter as in the example. ex) January 1, 2023 at 21:00 => 2023 01 01 21 00"
+            )
+            sys.exit()
+        if not re.fullmatch(TTL_REGEXP, ttl):
+            print(
+                "The time for the toast to stay alive format is incorrect, please enter 0 ~ 99."
+            )
+            sys.exit()
+
+        json_file = open("./setting.json", "r")
+        redis_setting = json.load(json_file)
+
         self.name = name
         self.message = message
         self.time = time
         self.ttl = ttl
-        self.redis = redis.Redis(host=LOCALHOST, port=PORT, db=DB)
+        self.redis = redis.Redis(
+            host=redis_setting["redis"]["localhost"],
+            port=redis_setting["redis"]["port"],
+            db=redis_setting["redis"]["db"],
+        )
 
     def register(self):
         self.redis.hset(f"Toast:{self.name}", "name", self.name)
